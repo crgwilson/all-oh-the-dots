1c0594e860514b42307292836b8ebd3995bc4c9a
Craig Wilson <craigjwilson93@gmail.com>
2020-08-01 16:40:39 -0400

    Add some golang aliases

--- zsh/26_go.zsh
+++ zsh/26_go.zsh
@@ -4,3 +4,9 @@
 
 export GOPATH=$HOME/go
 export PATH=$GOPATH/bin:$PATH
+
+alias gt='go test'
+alias gtb='go test -bench=.'
+alias gtc='go test -cover'
+alias gtr='go test -race'
+alias rungodoc='echo "Hosting docs on http://localhost:8000" && godoc -http ":8000" -goroot /usr/share/go'
